63a448cbfb31321091d4e78574db20c5a59e4c59
Tiffany678 <52tiffany.yang@gmail.comgit config --global user.name Tiffany678git config --global user.email 52tiffany.yang@gmail.com>
2022-12-12 00:43:27 -0500

    finished 4 method: add, remove, contains, find and size

--- src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
+++ src/main/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedList.java
@@ -4,4 +4,80 @@ package com.zipcodewilmington.singlylinkedlist;
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedList {
+    Node head;
+    Node tail;
+    public SinglyLinkedList() {
+        head = null;
+        tail = null;
+    }
+    // Node Class
+    class Node
+    {
+        int data;
+        Node next;
+
+        // Constructor to create
+        // a new node
+        Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+    }
+    public void add(int element){
+        Node newNode = new Node(element);
+        //check if the lined list is empty/null;
+        if(head==null) {
+            head = newNode;
+        }
+        if(tail==null){
+            tail=newNode;
+        }else {
+            tail.next = newNode;
+            tail = newNode;
+        }
+    }
+    public void remove(int elementIndex){
+        Node temp = head;
+
+        for(int i=0; i<elementIndex-1; i++){
+            temp = temp.next;
+        }
+        if(temp.next!=null) {
+            temp.next = temp.next.next;
+        }
+    }
+    public boolean contain(int element){
+        Node temp = head;
+        while (temp!=null){
+            if(temp.data == element) {
+                return true;
+            }
+            temp=temp.next;
+        }
+        return false;
+    }
+    public int find(int element){
+        Node temp = head;
+        int counter=0;
+        while (temp!=null && temp.next!=null){
+            if(temp.data == element) {
+                return counter;
+            }
+            counter++;
+        }
+        return -1;
+    }
+    public int size(){
+        Node temp= head;
+        int counter =0;
+        while(temp!=null){
+            counter++;
+            temp=temp.next;
+        }
+        return counter;
+    }
+
+
+
 }
--- src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
+++ src/test/java/com/zipcodewilmington/singlylinkedlist/SinglyLinkedListTest.java
@@ -1,7 +1,44 @@
 package com.zipcodewilmington.singlylinkedlist;
 
+import org.junit.jupiter.api.Test;
+
+import javax.xml.soap.Node;
+
+import java.util.LinkedList;
+
+import static com.ibm.cuda.CudaError.Assert;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 /**
  * Created by leon on 1/10/18.
  */
 public class SinglyLinkedListTest {
+   @Test
+    public void testAdd(){
+        //Give
+      SinglyLinkedList singleNode = new SinglyLinkedList();
+      singleNode.add(1);
+
+       assertEquals(1,singleNode.size() );
+    }
+    @Test
+    public void testRemove(){
+        //Give
+        SinglyLinkedList singleNode = new SinglyLinkedList();
+        singleNode.add(1);
+        singleNode.add(3);
+        singleNode.remove(1);
+
+        assertEquals(1,singleNode.size() );
+    }
+    @Test
+    public void testContain(){
+        //Give
+        SinglyLinkedList singleNode = new SinglyLinkedList();
+        singleNode.add(1);
+        singleNode.add(3);
+       // singleNode.remove(1);
+
+        assertEquals(true,singleNode.contain(3) );
+    }
 }
